9bdd94a9c3438e59709177791d994145186f8204
Pabi691 <pabitra691@gmail.com>
2025-12-10 20:39:30 +0530

    new update

--- src/pages/Instructors.jsx
+++ src/pages/Instructors.jsx
@@ -11,17 +11,17 @@ const Instructors = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Instructors" />
-        <GridComponent
-          dataSource={instructorsData}
-          enableHover={false}
-          width="auto"
-          allowPaging
-          allowSorting
-          pageSettings={{ pageCount: 5 }}
-          selectionSettings={selectionsettings}
-          editSettings={editing}
-          toolbar={toolbarOptions}
-        >
+      <GridComponent
+        dataSource={instructorsData}
+        enableHover={false}
+        width="auto"
+        allowPaging
+        allowSorting
+        pageSettings={{ pageCount: 5 }}
+        selectionSettings={selectionsettings}
+        editSettings={editing}
+        toolbar={toolbarOptions}
+      >
         <ColumnsDirective>
           {/* eslint-disable-next-line react/jsx-props-no-spreading */}
           {instructorsGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
